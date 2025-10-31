71627c7e805d41130621eec20333e7902f41707e
Jefferson Lima <jeffersoninacio@hotmail.com>
2025-10-31 10:09:06 -0300

    Add database connection and query utility functions using pg

---
+++ src/database/index.ts
@@ -0,0 +1,33 @@
+import pg from 'pg';
+
+const databaseUrl = process.env.DATABASE_URL;
+
+if (!databaseUrl) {
+  throw new Error('DATABASE_URL is not set');
+}
+
+export const pool = new pg.Pool({ connectionString: databaseUrl });
+
+export async function query<T extends pg.QueryResultRow = pg.QueryResultRow>(
+  text: string,
+  params?: unknown[]
+): Promise<pg.QueryResult<T>> {
+  return pool.query<T>(text, params);
+}
+
+export async function withTransaction<T>(
+  fn: (client: pg.PoolClient) => Promise<T>
+): Promise<T> {
+  const client = await pool.connect();
+  try {
+    await client.query('BEGIN');
+    const result = await fn(client);
+    await client.query('COMMIT');
+    return result;
+  } catch (err) {
+    await client.query('ROLLBACK');
+    throw err;
+  } finally {
+    client.release();
+  }
+}
