e5eb85d77f0d2fbc0941a5538cf7197b2333dce8
Jefferson Lima <jeffersoninacio@hotmail.com>
2025-11-01 21:30:36 -0300

    Refactor server.ts to use uppercase constant for PORT and simplify main function execution

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,14 @@
 import { buildApp } from './app.js';
 
-const port = Number(process.env.PORT ?? 3000);
+const PORT = Number(process.env.PORT ?? 3000);
 
-async function main() {
+void (async () => {
   const app = await buildApp();
   try {
-    await app.listen({ host: '0.0.0.0', port });
-    app.log.info(`HTTP server listening on http://localhost:${port}`);
+    await app.listen({ host: '0.0.0.0', port: PORT });
+    app.log.info(`HTTP server listening on http://localhost:${PORT}`);
   } catch (err) {
     app.log.error(err);
     process.exit(1);
   }
-}
-
-main();
+})();
